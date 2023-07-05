c53a7151492945ebc1b59a64f671eb3157f68213
alexikiphenix <alexikiphenix@gmail.com>
2023-07-06 00:17:14 +0200

    dial v0.8

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,14 @@
 const wrapper = document.getElementById('wrapper');
 const dial = document.createElement('div');
 
+dial.setAttribute('id', 'dial');
 dial.style.border = "1px solid black";
 dial.style.backgroundColor = "#edf";
+
+for(let i = 0; i < 10; i++)
+{
+    const dialKey = document.createElement('div');
+}
+
 
 wrapper.appendChild(dial);
